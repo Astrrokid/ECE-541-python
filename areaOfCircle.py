cac39f3628a994ce5db5b8413a84581543585a04
Chidalu Nsobundu <kidstarro22@gmail.com>
2022-11-24 00:15:04 +0100

    assignment done

---
+++ areaOfCircle.py
@@ -0,0 +1,5 @@
+RadiusofC = float(input("what's the radius of the circle:"))
+
+AreaOfC = 22/7*RadiusofC**2
+
+print(AreaOfC)
---
+++ info.py
@@ -0,0 +1,5 @@
+NameOfP = input("what's your name?:")
+AgeOfP = input("input your age:")
+DeptOfP = input(" what's the name of your department?:")
+
+print(f" name = {NameOfP} \n age = {AgeOfP} \n department ={DeptOfP}")
